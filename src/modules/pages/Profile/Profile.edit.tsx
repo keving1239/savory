9ff65aa14b1f9aece957e7ce4981af5b3b8e4982
Kevin Galdamez <kevin.galdamez@credera.com>
2024-02-06 14:37:55 -0500

    Adding Edit Profile Component

--- src/modules/pages/Profile/Profile.edit.tsx
+++ src/modules/pages/Profile/Profile.edit.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
-import { Typography, Grid, Button, Stack, Card, CardContent, CardMedia, Container } from '@mui/material';
+import React, { useState } from 'react';
+import { Typography, Grid, Button, TextField, Card, CardContent, CardMedia, Container } from '@mui/material';
 
 const ProfileEdit = () => {
-    return;
+    const tile = {name: 'Johe Doe', imgSrc: '', bio: 'ajl;fdkja;lsdfkjas;ldfksjal;sdkfjal;skdfjl;aksfdj;lakdfjsl;aksdjfl;afkdjl;afsdjkjfadlk;ajdfkl;ajdfkaoldf;jfads'};
+    const [blogName, setBlogName] = useState(tile.name);
+    const [blogImg, setBlogImg] = useState(tile.imgSrc);
+    const [blogBio, setBlogBio] = useState(tile.bio);
+    const handleProfileEdits = () => {
+        tile.name = blogName;
+        tile.imgSrc = blogImg;
+        tile.bio = blogBio;
+        console.log(tile);
+    }
+
+
+    return (
+        <Card style={{ maxWidth: 400, margin: '0 auto', marginTop: 50, marginBottom: 50, padding: 10 }}>
+            <CardContent>
+            <form onSubmit={handleProfileEdits}>
+            <TextField 
+                type="file"
+                label="Blog Image"
+                variant='outlined'
+                fullWidth
+                margin='normal'
+                InputLabelProps={{ shrink: true }}  
+                onChange={(e) => {setBlogImg(e.target.value)}}
+            />
+            <TextField
+                label="Blog Name"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={blogName}
+                onChange={(e) => {setBlogName(e.target.value)}}/>
+            <TextField
+                label="Blog Bio"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={blogBio}
+                onChange={(e) => {setBlogBio(e.target.value)}}/>    
+            </form>
+            </CardContent>
+            <Button type="submit" variant="contained" color="primary" onClick={handleProfileEdits}>Update</Button>
+        </Card>
+    );
 }
 
 export default ProfileEdit;
