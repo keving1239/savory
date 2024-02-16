432d582d114c10e333186033b66a40bcc3ab47ae
Adhrik Chethan (Credera) <adhrik.chethan@credera.com>
2024-02-16 12:18:25 -0600

    Working on test

--- SavoryApp/src/modules/pages/Post/Post.create.test.tsx
+++ SavoryApp/src/modules/pages/Post/Post.create.test.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import {screen } from '@testing-library/react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { MemoryRouter } from 'react-router-dom';
 import PostCreate from './Post.create';
+import { Provider } from 'react-redux';
+import Post from './Post';
+
+
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useNavigate: jest.fn(),
   }));
   
-  describe('PostCreate component', () => {
+  describe('PostCreate component', () => 
+  {
     it('renders without crashing', () => {
       render(
         <MemoryRouter>
@@ -20,3 +26,4 @@ jest.mock('react-router-dom', () => ({
   
     // Other tests...
   });
+
