2d60e61de3553dc9dee9095fe76252bf72bf9ff9
Adhrik Chethan (Credera) <adhrik.chethan@credera.com>
2024-02-19 12:15:03 -0600

    Only really edited Post.create.test.tsx. Other files should not have major changes

--- SavoryApp/src/modules/pages/Post/Post.create.test.tsx
+++ SavoryApp/src/modules/pages/Post/Post.create.test.tsx
@@ -7,9 +7,53 @@ import PostCreate from './Post.create';
 import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
 import Error404 from '../../shared/Error404';
+import Settings from '../Profile/Settings';
+import ProfileTile, {TileProps} from '../Profile/Profile.tile';
 
+describe('ProfileTile', () => {
+  const defaultProps: TileProps = 
+  {
+    username: 'John Doe',
+    img: '/profile-image.jpg',
+    bio: 'A sample bio.',
 
+  };
+  test('renders ProfileTile component with default props', () => {
+    const { getByText, getByAltText } = render(<ProfileTile {...defaultProps} />);
 
+    // Verify that the rendered content includes the provided username, bio, and image alt text
+    getByText(defaultProps.username);
+    getByText(defaultProps.bio);
+    getByAltText(defaultProps.username);
+  });
+
+  test('renders ProfileTile component with custom props', () => {
+    const customProps: TileProps = {
+      username: 'Custom User',
+      img: '/custom-image.jpg',
+      bio: 'Custom bio.',
+    };
+
+    const { getByText, getByAltText } = render(<ProfileTile {...customProps} />);
+
+    // Verify that the rendered content includes the custom username, bio, and image alt text
+    getByText(customProps.username);
+    getByText(customProps.bio);
+    getByAltText(customProps.username);
+  });
+
+  test('renders ProfileTile component with default image if no image provided', () => {
+    const propsWithoutImg: TileProps = {
+      username: 'No Image User',
+      bio: 'No image bio.',
+    };
+
+    const { getByAltText } = render(<ProfileTile {...propsWithoutImg} />);
+
+    // Verify that the rendered content includes the default image alt text
+    getByAltText(propsWithoutImg.username);
+  });
+});
 
 
 
@@ -33,6 +77,16 @@ jest.mock('react-router-dom', () => ({
       render(<Error404/>);
       screen.debug();
     });
+
+    it('should render the Settings Page', () => {
+      render(<Settings/>);
+      screen.debug();
+    });
+
+    
+
+
+
   });
 
 
