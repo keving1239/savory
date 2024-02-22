1f6cdca0cca354ac44c70937ed6a7a0334066746
Adhrik Chethan (Credera) <adhrik.chethan@credera.com>
2024-02-22 13:31:22 -0600

    Commiting to test master

--- SavorySpringAPI/src/test/java/com/savory/savoryAPI/post/PostControllerIntTest.java
+++ SavorySpringAPI/src/test/java/com/savory/savoryAPI/post/PostControllerIntTest.java
@@ -3,14 +3,19 @@ package com.savory.savoryAPI.post;
 import com.savory.savoryAPI.post.dto.PostsDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,14 +27,20 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(value = PostController.class)
+//@WebMvcTest(value = PostController.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+
+
 //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 //@TestPropertySource(locations = "classpath:application.properties")
+//@ContextConfiguration(locations = "classpath:applicationContext.xml")
 class PostControllerIntTest
 {
 
@@ -40,22 +51,15 @@ class PostControllerIntTest
     @Autowired
     private PostService postService; // Autowire the actual PostService
 
-    @Autowired
-    private PostRepository postRepository;
-
-    @Autowired
-    private PostsURepository postsURepository;
+//    @Autowired
+//    private PostRepository postRepository;
+//
+//    @Autowired
+//    private PostsURepository postsURepository;
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Configuration
-    static class TestConfig {
-        @Bean
-        public PostService postService(PostRepository postRepository, PostsURepository postsURepository) {
-            return new PostService(postRepository, postsURepository); // Create an instance of PostService
-        }
-    }
 
 
 
@@ -65,7 +69,8 @@ class PostControllerIntTest
 
 
 
-        @Test
+
+    @Test
     void getPostIfIDInSetIntTest() throws Exception
     {
 //        List<PostsDto> samplePostsList = Arrays.asList(
@@ -73,17 +78,16 @@ class PostControllerIntTest
 //        );
 
         // Define the behavior of postService.findAllPosts() when called in the controller
-//        Mockito.when(postService.findAllPosts()).thenReturn(samplePostsList);
+//        Mockito.when(postService.findAllPosts()).thenReturn(Collections.emptyList());
 
-        RequestBuilder request = MockMvcRequestBuilders.get("/posts/all")
-                .contentType(MediaType.APPLICATION_JSON);
+        RequestBuilder request = MockMvcRequestBuilders.get("/posts/all");
+//                .contentType(MediaType.APPLICATION_JSON);
 
         MvcResult result = mvc.perform(request)
                 .andExpect(status().isOk()) // Expecting HTTP status 200
                 .andReturn();
 
         String responseBody = result.getResponse().getContentAsString();
-//        List<PostsDto> postsList = objectMapper.readValue(responseBody, new TypeReference<>() {});
 
 //        Mockito.verify(postService, Mockito.times(1)).findAllPosts();
         assertEquals(200, result.getResponse().getStatus());
@@ -92,6 +96,35 @@ class PostControllerIntTest
 //        System.out.println(postsList);
         System.out.println(responseBody);
     }
+
+    @Test
+    void getPostIfIDInSetIntTestForUsername() throws Exception
+    {
+//        List<PostsDto> samplePostsList = Arrays.asList(
+//                createExpectedPostDto(31, 3, "Parmesian","Test", "Test", "cheesy", "1999")
+//        );
+
+        // Define the behavior of postService.findAllPosts() when called in the controller
+//        Mockito.when(postService.findAllPosts()).thenReturn(Collections.emptyList());
+
+        RequestBuilder request = MockMvcRequestBuilders.get("/posts/allWithUsername");
+//                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mvc.perform(request)
+                .andExpect(status().isOk()) // Expecting HTTP status 200
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+//        Mockito.verify(postService, Mockito.times(1)).findAllPosts();
+        assertEquals(200, result.getResponse().getStatus());
+        System.out.println(result.getResponse().getStatus());
+
+//        System.out.println(postsList);
+        System.out.println(responseBody);
+    }
+
+
 
 
 }
