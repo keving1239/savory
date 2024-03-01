bc93a220489845c786ffcb6ede081ffe86c3f808
Sanjit Muthineni <sanjit.muthineni@credera.com>
2024-03-01 10:53:01 -0500

    edit validation

--- SavoryApp/src/modules/pages/Post/Post.edit.tsx
+++ SavoryApp/src/modules/pages/Post/Post.edit.tsx
@@ -73,12 +73,31 @@ const PostEdit = ({recipeItem}: {recipeItem?: Recipe}) => {
     useEffect(() => {
         loadRecipe();
     }, [id]); // Called any time id changes
-    
+      // form validation
+    const validateFields = () => {
+        const titleRegex = /^[\w /\(\)\-\.]{3,100}$/.test(title);
+        setTitleError((!titleRegex) ? 
+        {error: true, helperText: 'Must be 3-100 characters & only contain letters, numbers, space, _-./()'}
+        : {error: false, helperText: ''}
+        );
+        const ingredientsRegex = /^(([\w /\(\)\-\.]){1,63}(\n|,|$)){1,63}$/.test(ingredients);
+        setIngredientsError((!ingredientsRegex) ? 
+        {error: true, helperText: 'Must follow the format in ingredients tool tip only contain letters, numbers, space, _-./()'}
+        : {error: false, helperText: ''}
+        );
+        const tagsRegex = /^((#([a-zA-Z0-9\-]){1,31}),{0,1}){1,63}$/.test(tags);
+        setTagsError((tags && !tagsRegex) ?  
+        {error: true, helperText: 'Must follow the format in tags tool tip & contain only letters, numbers, -'}
+        : {error: false, helperText: ''}
+        );
+        return (tags && tagsRegex) && ingredientsRegex && titleRegex && recipe;
+    }
+
     // form submission
     const handlePostEdit = async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-    //   const isValid = validateFields();
-      if(!userId) return;
+      const isValid = validateFields();
+      if(!userId || !isValid) return;
       await dispatch(updateRecipe(
         {
             userId: userId, headline: title, ingredients: ingredients, recipe: recipe, img: img, tags: tags,
@@ -98,27 +117,7 @@ const PostEdit = ({recipeItem}: {recipeItem?: Recipe}) => {
         setOpen(false);
         navigate('/feed');
     }
-  
-    // props for each textfield
-    // const fields: {tip: string, size: number, fieldProps: TextFieldProps}[] = [
-    //   {tip: 'Name of your food', size: 12, 
-    //     fieldProps: {value: title, type: 'text', onChange: (e) => setTitle(e.target.value), error: titleError.error, helperText: titleError.helperText,
-    //     label: 'Title', placeholder: 'Southwest Salad', required: true, variant: 'outlined', margin: 'dense', fullWidth: true}},
-    //   {tip: 'URL for a picture of your food', size: 12,
-    //     fieldProps: {value: img, type: 'text', onChange: (e) => setImg(e.target.value),
-    //     label: 'Image', placeholder: 'https://images.unsplash.com/...', required: true,}},
-    //   {tip: 'Ingredients in this food (list each ingredient seperated by new line or commas)', size: 5.9,
-    //     fieldProps: {value: ingredients, type: 'text', onChange: (e) => setIngredients(e.target.value), error: ingredientsError.error, helperText: ingredientsError.helperText,
-    //     label: 'Ingredients', placeholder: '1 Head Romaine Lettuce\n12 Cherry Tomatoes\n1 Can Black Beans\n...', 
-    //     required: true, multiline: true, minRows: 4, maxRows: 4, variant: 'outlined', margin: 'dense', fullWidth: true}},
-    //   {tip: 'Instructions to make this food', size: 5.9,
-    //     fieldProps: {value: recipe, type: 'text', onChange: (e) => setRecipe(e.target.value),
-    //     label: 'Recipe', placeholder: 'First, prepare your vegetables. Next, Drain and rinse the black beans.', 
-    //     required: true, multiline: true, minRows: 4, maxRows: 4, variant: 'outlined', margin: 'dense', fullWidth: true}},
-    //   {tip: 'One word hashtags seperated by commas', size: 12,
-    //     fieldProps: {value: tags, type: 'text', onChange: (e) => setTags(e.target.value), error: tagsError.error, helperText: tagsError.helperText,
-    //     label: 'Tags', placeholder: '#TexMex, #Refreshing, #Healthy', variant: 'outlined', margin: 'dense', fullWidth: true}},
-    // ];
+
     return ( 
         post ? (    
             <Box display='flex' justifyContent='center' alignItems='center' minHeight='70vh'>
