ad385847660135e272c0de5e0edb212ffe1ea111
Kevin Galdamez <kevin.galdamez@credera.com>
2024-03-06 15:58:55 -0500

    Updated Profile.test.tsx to match changes in setupTests.tsx

--- SavoryApp/src/modules/pages/Profile/Profile.test.tsx
+++ SavoryApp/src/modules/pages/Profile/Profile.test.tsx
@@ -1,130 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { screen, waitFor } from '@testing-library/react';
+import { Route, Routes } from 'react-router-dom';
 import Profile from './Profile';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import { RootState } from '../../../redux/store';
-import StandardLayout from '../../StandardLayout';
-import { ThemeProvider } from '@emotion/react';
-import createAppTheme from '../../../app/App.theme';
-import { UserState } from '../../../redux/User/user-slice';
-import { LocalRecipesState } from '../../../redux/Recipes/recipes-slice';
-import { InteractionsState } from '../../../redux/Interactions/interactions-slice';
-import { useSelector } from 'react-redux';
-import Feed from '../../shared/Feed';
+import { authState, renderWithProviders } from '../../../setupTests';
 
-/*
-global.fetch = jest.fn(() => Promise.resolve({
-    json: () => Promise.resolve({
-            id: 12,
-            username: 'batman',
-            img: '',
-            bio: 'im batman'
-        }),
-    })) as jest.Mock;
-    */
-
-(global as any).fetch = jest.fn();
-
-const mockUser = {
-    id: 12,
-    username: 'batman',
-    email: 'test@example.com',
-    img: 'test.jpg',
-    bio: 'Test bio'
-};
-
-// Create a mock Redux store with the desired initial state
-const mockStore = configureStore<RootState>([]);
-const initialState: RootState = {
-    persistedReducer: {
-        userReducer: {
-            user: mockUser,
-            isAuthenticated: true,
-            isAdmin: false
-        },
-        recipesReducer: {
-            recipes: {},
-            page: 1,
-            sort: "A"
-        },
-        interactionsReducer: {
-            interactions: {},
-        },
-        themeReducer: {
-            mode: "light"
-        },
-        _persist: {
-            version: -1,
-            rehydrated: true,
-        }
-    }
-};
-
-const store = mockStore(initialState);
-
-/*
-  interface FakeState {
-    persistedReducer: {
-        userReducer: UserState;
-        recipesReducer: LocalRecipesState;
-        interactionsReducer: InteractionsState;
-    };
-  }
-  const fakeState: FakeState = {
-    persistedReducer: {
-      userReducer: {
-        isAuthenticated: true,
-        isAdmin: false,
-        user: null,
-        token: null,
-      } as UserState,
-      recipesReducer: {
-        recipes: {}, // Add fake recipes as needed
-      } as LocalRecipesState,
-      interactionsReducer: {
-        interactions: {}, // Add fake interactions as needed
-      } as InteractionsState,
-    }
-  };
-  const mockStore = configureStore([]);
-  */
-
-const FeedComponent = () => {
-    return <div>Avocado Toast</div>
-};
-
+// (global as any).fetch = jest.fn();
 
 describe('Profile', () => {
-
-    // other parts of the RootState as needed
-
-    (global as any).fetch = jest.fn();
-
-    /*
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve({
-              id: 12,
-              username: 'batman',
-              img: '',
-              bio: 'im batman'
-          }),
-      })) as jest.Mock;
-      */
-
     beforeEach(() => {
-        const username = 'batman'
+        const username = authState.userReducer?.user?.username || '';
         jest.spyOn(global, 'fetch').mockImplementation((input: RequestInfo | URL, init?: RequestInit) => {
             const url = typeof input === 'string' ? input : (input as Request).url;
-
             if (url === `http://localhost:8080/api/person/byUsername/${username}`) {
                 return Promise.resolve({
                     json: () => Promise.resolve({
-                        id: 12,
-                        username: 'batman',
+                        id: 205,
+                        username: 'savory.taste.tester',
                         img: '',
-                        bio: 'im batman'
+                        bio: 'Hi, I am a tester for Savory!'
                     })
                 } as Response);
             } else if (url === `http://localhost:8080/api/posts/byUserId/12?pageNumber=1`) {
@@ -146,24 +39,14 @@ describe('Profile', () => {
             }
         });
     });
-
-    it('loads the correct username', async () => {
-        const route = `/profile/batman`;
-
-        render(
-            <Provider store={store}>
-                <ThemeProvider theme={createAppTheme("light")}>
-                    <MemoryRouter initialEntries={[route]}>
-                        <Routes>
-                            <Route path="/profile/:username" element={<Profile />} />
-                        </Routes>
-                    </MemoryRouter>
-                </ThemeProvider>
-            </Provider>
-        );
-
-        const usernameElement = await screen.findByText(`batman`);
-        //   expect(screen.getByText("Avocado")).toBeInTheDocument();
-        expect(usernameElement).toBeInTheDocument();
+    it('loads the correct username', () => {
+        const bio = authState.userReducer?.user?.bio || '';
+        const username = authState.userReducer?.user?.username || '';
+        renderWithProviders(
+            <Routes>
+                <Route path={`/profile/username${username}`} element={<Profile />} /> 
+            </Routes>,
+        {preloadedState: authState});
+        waitFor(() => {expect(screen.findByText(bio)).toBeInTheDocument();});
     });
 });
