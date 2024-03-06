4f2d5a3025be4a8dfcdeb0623f0fd87a147feb65
Kevin Galdamez <kevin.galdamez@credera.com>
2024-03-06 14:00:31 -0500

    Auth Verifier now ensures jwt is from our Auth0 JWT Issuer

--- SavorySpringAPI/src/main/java/com/savory/savoryAPI/authorization/AuthVerifier.java
+++ SavorySpringAPI/src/main/java/com/savory/savoryAPI/authorization/AuthVerifier.java
@@ -11,6 +11,8 @@ import java.util.List;
 public class AuthVerifier {
     @Value("${auth0.namespace}")
     private String namespace;
+    @Value("${auth0.domain}")
+    private String domain;
 
     private final JwtDecoder decoder;
 
@@ -32,9 +34,15 @@ public class AuthVerifier {
         return jwt.getClaimAsString(namespace+"roles");
     }
 
+    public String getIssuer(Jwt jwt) {
+        if(jwt == null) return "";
+        return jwt.getClaimAsString("iss");
+    }
+
     public Authentication getAuthentication(String token) {
         Jwt jwt = validateJwtToken(token);
-        String email = getEmail(jwt), roles = getRoles(jwt);
+        String email = getEmail(jwt), roles = getRoles(jwt), iss = getIssuer(jwt);
+        if(email.isEmpty() || !iss.equals(domain)) return null;
         if(roles.isEmpty()) roles = "ANONYMOUS";
         List<String> rolesList = List.of(roles);
         List<SimpleGrantedAuthority> authorities = rolesList.stream()
