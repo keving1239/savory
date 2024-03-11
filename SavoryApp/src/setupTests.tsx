a14e84546cadf06c8e1d45bfad15e507b43edcfa
Kevin Galdamez <92188733+keving1239@users.noreply.github.com>
2024-03-11 11:25:12 -0400

    Update setupTests.tsx

--- SavoryApp/src/setupTests.tsx
+++ SavoryApp/src/setupTests.tsx
@@ -23,7 +23,7 @@ Object.defineProperty(window, 'crypto', {
         getRandomValues: jest.fn(),
     },
 });
-export const testToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxnTXlwWWkweTNzbU5FdTdZUVpVayJ9.eyJodHRwOi8vbG9jYWxob3N0OjgwODAvc2F2b3J5L2luaXR0b3dpbml0L2VtYWlsIjoidGFzdGUudGVzdGVyQHNhdm9yeS5jb20iLCJodHRwOi8vbG9jYWxob3N0OjgwODAvc2F2b3J5L2luaXR0b3dpbml0L3JvbGVzIjoiIiwiaXNzIjoiaHR0cHM6Ly9kZXYtdDZ2c3B1YzhxcnNzYWFyYy51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjVlOGE5YmE1MTRlZDU0ZGEwYjE0MDYyIiwiYXVkIjpbImh0dHBzOi8vZGV2LXQ2dnNwdWM4cXJzc2FhcmMudXMuYXV0aDAuY29tL2FwaS92Mi8iLCJodHRwczovL2Rldi10NnZzcHVjOHFyc3NhYXJjLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE3MDk3NDc1NTcsImV4cCI6MTcwOTgzMzk1NywiYXpwIjoiUU9objQ3SThWUXBDZDNRUzhyOTh1NnFVWkFqVkNKS3QiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.azYcv8vb8Y3PsozDzzR3uhwmrM6s1yOUyH2d6LeEYbqP2DTaGvfNbqrZ3EkX2rZcvxqQV6UZXq87ieClgeoG9l57NBAo_eDeZdT2T-RSJ6afI23RWO1StAGgW7wjL9I4XoX6_Ez6JBqKi8yoF8vvGoOZfX1OvfCUX_VGyi0LweyET6sUq0d58loSI2qv_a8PQd6iWnHyAhabbp5GSfw4wx6OiY63yM8Q6mscHY1S9AqYr3n7RCMDaL42X1Zcd3rLEm36q94pZLrMxchS8WUqXStJ_IERHUGhto0hzVbhdgsQNiVqo5CG9YOxmTYYoU4Ozx5VVHF6m2XwVbVVKE6-vw';
+export const testToken = 'token';
 jest.mock('@auth0/auth0-spa-js', () => ({
     ...jest.requireActual('@auth0/auth0-spa-js'),
     isAuthenticated: true,
@@ -60,7 +60,6 @@ export const authState = {
         user: {
             id: 105,
             username: 'savory.taste.tester',
-            // password: @iHV2LId!1EqDAeQ
             email: 'taste.tester@savory.com',
             img: '',
             bio: 'Hi, I am a tester for Savory!',
@@ -100,8 +99,8 @@ export function renderWithProviders(
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistStore(store)}>
             <Auth0Provider
-            domain="dev-t6vspuc8qrssaarc.us.auth0.com"
-            clientId="QOhn47I8VQpCd3QS8r98u6qUZAjVCJKt"
+            domain="auth0domain"
+            clientId="clientid"
             authorizationParams={{
                 redirect_uri: `${window.location.origin}/login`,
                 scope: 'openid profile email'
